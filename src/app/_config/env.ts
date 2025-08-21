82fd35fcc8fead3137ff197a6fb836ca317f7b92
James <john.usa.james@gmail.com>
2025-08-21 18:53:23 +0100

    fix: change USE_MEMORY variable type to coerced string in EnvSchema

--- src/app/_config/env.ts
+++ src/app/_config/env.ts
@@ -4,7 +4,7 @@ import {z} from "zod"
 const EnvSchema = z.object({
   NEXT_PUBLIC_SUPABASE_URL: z.url().optional().default(""), // TODO: Remove .optional().default("")
   NEXT_PUBLIC_SUPABASE_ANON_KEY: z.string().optional().default(""), // TODO: Remove .optional().default("")
-  USE_MEMORY: z.string().optional().default("0"),
+  USE_MEMORY: z.coerce.string().optional().default("0"),
 })
 
 export const env = EnvSchema.parse(process.env)
