4196a64312f98a9e833119f298c2ae5bd19a044e
James <john.usa.james@gmail.com>
2025-08-21 18:27:32 +0100

    fix: add default values to NEXT_PUBLIC_SUPABASE_URL and NEXT_PUBLIC_SUPABASE_ANON_KEY in EnvSchema

--- src/app/_config/env.ts
+++ src/app/_config/env.ts
@@ -2,8 +2,8 @@ import {z} from "zod"
 
 // A schema for environment variables. @see https://env.t3.gg/docs/core
 const EnvSchema = z.object({
-  NEXT_PUBLIC_SUPABASE_URL: z.url().optional(),
-  NEXT_PUBLIC_SUPABASE_ANON_KEY: z.string().optional(),
+  NEXT_PUBLIC_SUPABASE_URL: z.url().optional().default(""), // TODO: Remove .optional().default("")
+  NEXT_PUBLIC_SUPABASE_ANON_KEY: z.string().optional().default(""), // TODO: Remove .optional().default("")
   USE_MEMORY: z.string().optional().default("0"),
 })
 
