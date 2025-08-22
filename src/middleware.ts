8e3b915940d41045a44a2c790e0dba961583914e
John James <john.usa.james@gmail.com>
2025-08-22 02:39:57 +0100

    fix: update middleware matcher to exclude /api/health path

--- src/middleware.ts
+++ src/middleware.ts
@@ -15,8 +15,9 @@ export const config = {
      * - _next/static (static files)
      * - _next/image (image optimization files)
      * - favicon.ico (favicon file)
+     * - /api/health
      * Feel free to modify this pattern to include more paths.
      */
-    "/((?!_next/static|_next/image|favicon.ico|.*\\.(?:svg|png|jpg|jpeg|gif|webp)$).*)",
+    "/((?!_next/static|_next/image|favicon.ico|api/health|.*\\.(?:svg|png|jpg|jpeg|gif|webp)$).*)",
   ],
 }
