7257cafc7bb606ef82293acb803813d1282dc0e0
James <john.usa.james@gmail.com>
2025-08-24 00:18:47 +0100

    refactor: restore default values for optional environment variables in EnvSchema

--- src/app/_config/env.ts
+++ src/app/_config/env.ts
@@ -2,9 +2,9 @@ import {z} from "zod"
 
 // A schema for environment variables. @see https://env.t3.gg/docs/core
 const EnvSchema = z.object({
-  NEXT_PUBLIC_SUPABASE_URL: z.url().optional(),
-  NEXT_PUBLIC_SUPABASE_ANON_KEY: z.string().optional(),
-  SUPABASE_SERVICE_ROLE_KEY: z.string().optional(),
+  NEXT_PUBLIC_SUPABASE_URL: z.url().optional().default(""), // TODO: Remove .optional().default("")
+  NEXT_PUBLIC_SUPABASE_ANON_KEY: z.string().optional().default(""), // TODO: Remove .optional().default("")
+  SUPABASE_SERVICE_ROLE_KEY: z.string().optional().default(""), // TODO: Remove .optional().default("")
   USE_MEMORY: z.coerce.string().optional().default("0"),
 })
 
