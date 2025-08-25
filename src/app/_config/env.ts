dc4569aa9d6023be80c16c05d4da72c3e373f2bc
James <john.usa.james@gmail.com>
2025-08-25 12:06:38 +0100

    refactor: simplify variable name for GitHub Actions check in environment schema

--- src/app/_config/env.ts
+++ src/app/_config/env.ts
@@ -1,12 +1,12 @@
 import {z} from "zod"
 
-// A schema for environment variables. @see https://env.t3.gg/docs/core
-const isGitHubActions = process.env.GITHUB_ACTIONS === "true"
+const isGHA = process.env.GITHUB_ACTIONS === "true"
 
+// A schema for environment variables. @see https://env.t3.gg/docs/core
 const EnvSchema = z.object({
-  NEXT_PUBLIC_SUPABASE_URL: isGitHubActions ? z.url().default("") : z.url(),
-  NEXT_PUBLIC_SUPABASE_ANON_KEY: isGitHubActions ? z.string().default("") : z.string().min(1),
-  SUPABASE_SERVICE_ROLE_KEY: isGitHubActions ? z.string().default("") : z.string().min(1),
+  NEXT_PUBLIC_SUPABASE_URL: isGHA ? z.url().default("") : z.url(),
+  NEXT_PUBLIC_SUPABASE_ANON_KEY: isGHA ? z.string().default("") : z.string().min(1),
+  SUPABASE_SERVICE_ROLE_KEY: isGHA ? z.string().default("") : z.string().min(1),
 })
 
 export const env = EnvSchema.parse(process.env)
