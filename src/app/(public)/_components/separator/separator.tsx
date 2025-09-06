d2ea498f0c26b29d899d1cd06aa68581feec91bf
James <john.usa.james@gmail.com>
2025-09-06 13:41:01 +0100

    feat(separator): add Separator component using Radix UI

---
+++ src/app/(public)/_components/separator/separator.tsx
@@ -0,0 +1,29 @@
+import * as SeparatorPrimitive from "@radix-ui/react-separator"
+import type {ComponentProps} from "react"
+import {twMerge} from "tailwind-merge"
+
+export function Separator({
+  className,
+  orientation = "horizontal",
+  decorative = true,
+  ...props
+}: ComponentProps<typeof SeparatorPrimitive.Root>) {
+  const baseClassName = [
+    "bg-border", // Use theme border color
+    "shrink-0", // Prevent flexbox shrinking
+    "data-[orientation=horizontal]:h-px", // 1px height when horizontal
+    "data-[orientation=horizontal]:w-full", // Full width when horizontal
+    "data-[orientation=vertical]:h-full", // Full height when vertical
+    "data-[orientation=vertical]:w-px", // 1px width when vertical
+  ]
+
+  return (
+    <SeparatorPrimitive.Root
+      data-slot="separator"
+      decorative={decorative}
+      orientation={orientation}
+      className={twMerge(baseClassName, className)}
+      {...props}
+    />
+  )
+}
