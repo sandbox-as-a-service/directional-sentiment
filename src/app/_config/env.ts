42cadd27b1e3dd33a6d74ab6b7bc4af12aa13c40
James <john.usa.james@gmail.com>
2025-09-10 13:35:38 +0100

    feat(env): update environment variable schema and add Supabase configuration

--- src/app/_config/env.ts
+++ src/app/_config/env.ts
@@ -1,22 +1,20 @@
 import {z} from "zod"
 
-const isGHA = process.env.GITHUB_ACTIONS === "true"
+const flags = {
+  IS_DEV: process.env.NODE_ENV === "development",
+  IS_TEST: process.env.NODE_ENV === "test",
+  IS_PROD: process.env.NODE_ENV === "production",
+  IS_GHA: process.env.GITHUB_ACTIONS === "true",
+} as const
 
 // A schema for environment variables. @see https://env.t3.gg/docs/core
 const EnvSchema = z.object({
-  NODE_ENV: z.enum(["development", "test", "production"]),
-  NEXT_PUBLIC_SUPABASE_URL: isGHA ? z.url().default("") : z.url(),
-  NEXT_PUBLIC_SUPABASE_ANON_KEY: isGHA ? z.string().default("") : z.string().min(1),
-  SUPABASE_SERVICE_ROLE_KEY: isGHA ? z.string().default("") : z.string().min(1),
+  NEXT_PUBLIC_SUPABASE_URL: flags.IS_GHA ? z.url().default("") : z.url(),
+  NEXT_PUBLIC_SUPABASE_ANON_KEY: flags.IS_GHA ? z.string().default("") : z.string().min(1),
+  SUPABASE_SERVICE_ROLE_KEY: flags.IS_GHA ? z.string().default("") : z.string().min(1),
 })
 
 const raw = EnvSchema.parse(process.env)
-
-const flags = {
-  IS_DEV: raw.NODE_ENV === "development",
-  IS_TEST: raw.NODE_ENV === "test",
-  IS_PROD: raw.NODE_ENV === "production",
-} as const
 
 export const env = {
   ...raw,
