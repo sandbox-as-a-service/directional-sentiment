6066eaf80712f504bd1b9bbeacfb985441c6f7d6
James <john.usa.james@gmail.com>
2025-09-13 12:26:42 +0100

    feat(sidebar): implement Sidebar component with mobile support and menu items feat(sheet): add Sheet component for modal-like functionality feat(tooltip): create Tooltip component for enhanced user interaction feat(hooks): add useIsMobile hook for responsive design handling

---
+++ src/app/(public)/_components/sidebar/app-sidebar.tsx
@@ -0,0 +1,46 @@
+import {Calendar, Home, Inbox, Search, Settings} from "lucide-react"
+
+import {
+  Sidebar,
+  SidebarContent,
+  SidebarGroup,
+  SidebarGroupContent,
+  SidebarGroupLabel,
+  SidebarMenu,
+  SidebarMenuButton,
+  SidebarMenuItem,
+} from "./sidebar"
+
+const items = [
+  {
+    title: "Home",
+    url: "feed",
+    icon: Home,
+  },
+]
+
+export function AppSidebar() {
+  return (
+    <Sidebar className="z-50">
+      <SidebarContent>
+        <SidebarGroup>
+          <SidebarGroupLabel>Application</SidebarGroupLabel>
+          <SidebarGroupContent>
+            <SidebarMenu>
+              {items.map((item) => (
+                <SidebarMenuItem key={item.title}>
+                  <SidebarMenuButton asChild>
+                    <a href={item.url}>
+                      <item.icon />
+                      <span>{item.title}</span>
+                    </a>
+                  </SidebarMenuButton>
+                </SidebarMenuItem>
+              ))}
+            </SidebarMenu>
+          </SidebarGroupContent>
+        </SidebarGroup>
+      </SidebarContent>
+    </Sidebar>
+  )
+}
